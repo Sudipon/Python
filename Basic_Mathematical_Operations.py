5a5dabef34c8fd4d3d970d7f30bfdf747cc6a754
Sudipon <60188509+Sudipon@users.noreply.github.com>
2025-04-30 19:27:26 +0530

    Add files via upload

---
+++ Basic_Mathematical_Operations.py
@@ -0,0 +1,13 @@
+a = input("Enter the First Number: ")
+b = input("Enter the Second Number: ")
+a = int(a)
+b = int(b)
+Add = a + b
+Sub = a - b
+Mul = a * b
+Div = a / b
+print("\n")
+print("Addition: ", Add)
+print("Subtraction: ", Sub)
+print("Multiplication: ", Mul)
+print("Division: ", Div)
